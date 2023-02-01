af17136a3867982d7df8d14d83d1287a0e9a3858
unknown <dlscksgnl@naver.com>
2023-02-01 11:26:36 +0900

    write letter1

--- src/components/write_letter/WriteLetter.tsx
+++ src/components/write_letter/WriteLetter.tsx
@@ -1,18 +1,36 @@
 import React, { useState } from 'react'
 
 const WriteLetter = () => {
+    const BaseUrl = "http://localhost:8080/api/myLetter/createMyLetter";
     const [letter, setLetter] = useState({
-        nickName: '',
+        title: '',
         content: '',
         isCheck: false,
     });
-    const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
         setLetter({
             ...letter,
             [e.target.name]: e.target.value,
         })
     }
     const handleClick = () => {
+        let now = new Date();
+        let todayYear = now.getFullYear();
+        let todayMonth = now.getMonth() + 1;
+        let todayDate = now.getDate();
+        const nowDate = `${todayYear}-${todayMonth}-${todayDate}`;
+        fetch(BaseUrl, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                myLetterContent: letter.content,
+                myLetterCreatedWhen: nowDate,
+                myLetterId: `${sessionStorage.getItem('user_id')}-${letter.title}`,
+                myLetterIsPrivate: letter.isCheck,
+                myLetterTitle: letter.title,
+                writerId: null,
+            }),
+        });
         alert('전송이 완료되었습니다!');
         document.location.href = '/';
     }
@@ -30,6 +48,7 @@ const WriteLetter = () => {
                 <h1>편지 쓰기</h1>
             </div>
             <div className='mx-20 mt-16'>
+                <input type="text" onChange={handleChange} name='title' className='w-full mb-10 border-b-2 border-slate-500' placeholder='제목'/>
                 <textarea onChange={handleChange} name='content' value={letter.content} className='w-full border-b-2 border-black h-96' placeholder='오늘의 이야기를 들려주세요. :)' />
                 <div className='flex items-center justify-between mt-4 space-x-2'>
                     <div className='flex items-center'>

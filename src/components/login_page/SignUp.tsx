66a3b118c07cf9897ded361d7d8bee9c223d2c9b
wassaa-1 <dlscksgnl@naver.com>
2023-02-02 14:23:51 +0900

    sign up effectiveness

--- src/components/login_page/SignUp.tsx
+++ src/components/login_page/SignUp.tsx
@@ -11,15 +11,63 @@ const SignUp = () => {
         eMail: "",
     });
 
+    // 입력 값 유효성 검사
+    const [effectiveness, setEffectiveness] = useState({
+        id: false,
+        password: false,
+        verifyPassword: false,
+        nickName: false,
+        eMail: false,
+    })
+    const isId = (id: string): boolean => {
+        const idRegex =
+            /^\w{6,12}$/i;
+        return idRegex.test(id);
+    };
+    const isPassword = (password: string): boolean => {
+        const passwordRegex =
+            /^\w{8,12}$/i;
+        return passwordRegex.test(password);
+    };
+    const isVerifyPassword = (password: string, verifyPassword: string): boolean => {
+        return password == verifyPassword;
+    };
+    const isNickName = (nickName: string): boolean => {
+        const nickNameRegex =
+            /^[가-힣a-zA-Z0-9]{2,}$/;
+        return nickNameRegex.test(nickName);
+    };
+    const isEmail = (email: string): boolean => {
+        const emailRegex =
+            /^[a-z0-9_+.-]{3,}@([a-z0-9-]+\.)+[a-z0-9]{2,4}$/i;
+        return emailRegex.test(email);
+    };
+
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setValues({
             ...values,
             [e.target.name]: e.target.value,
         });
+        let isGood = false;
+        if (e.target.name === 'id') {
+            isGood = isId(e.target.value);
+        } else if (e.target.name === 'password') {
+            isGood = isPassword(e.target.value);
+        } else if (e.target.name === 'verifyPassword') {
+            isGood = isVerifyPassword(values.password, e.target.value);
+        } else if (e.target.name === 'nickName') {
+            isGood = isNickName(e.target.value);
+        } else if (e.target.name === 'eMail') {
+            isGood = isEmail(e.target.value);
+        }
+        setEffectiveness({
+            ...effectiveness,
+            [e.target.name]: isGood,
+        })
     };
 
-    const handleSubmit = () => {
-        if (values.password === values.verifyPassword) {
+    const handleSubmit = async () => {
+        if (effectiveness.id && effectiveness.password && effectiveness.verifyPassword && effectiveness.nickName && effectiveness.eMail) {
             let now = new Date();
             let todayYear = now.getFullYear();
             let todayMonth = now.getMonth() + 1;
@@ -27,7 +75,7 @@ const SignUp = () => {
             let hours = now.getHours();
             let minutes = now.getMinutes();
             const nowDate = `${todayYear}-${todayMonth}-${todayDate} ${hours}:${minutes}`;
-            fetch(BaseUrl, {
+            const message = await fetch(BaseUrl, {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({
@@ -38,12 +86,17 @@ const SignUp = () => {
                     userNotify: true,
                     userPw: values.password,
                 }),
-            });
-
-            alert("이제 로그인 하여 We로 서비스를 이용해보세요!!");
-            document.location.href = "/";
+            })
+                .then(req => req.json())
+                .then(data => String(data.message));
+            if (message === '이미 존재하는 ID 입니다.') {
+                alert(message);
+            } else {
+                alert("이제 로그인 하여 WeRo를 이용해보세요!!");
+                document.location.href = "/";
+            }
         } else {
-            alert("Verify Password에 Password와 같은 값을 입력해 주세요.");
+            alert('아직 완성되지 않은 부분이 있어요!')
         }
     };
 
@@ -56,6 +109,8 @@ const SignUp = () => {
             handleSubmit();
         }
     };
+
+    const commonStyle = 'h-12 pl-2 my-3 font-mono text-lg w-96 border-b-2';
 
     return (
         <div className="flex flex-col items-center">
@@ -75,10 +130,16 @@ const SignUp = () => {
                                 name="id"
                                 value={values.id}
                                 onChange={handleChange}
-                                className="h-12 gap-2 pl-2 my-3 font-mono text-lg border-b-2 border-gray-400 border-solid w-96"
-                                placeholder="ID"
+                                className={effectiveness.id ?
+                                    `${commonStyle} border-gray-400 border-solid` : `${commonStyle} border-red-700 border-solid`}
+                                placeholder='ID'
                                 ref={focusRef}
                             />
+                            {
+                                effectiveness.id === false ?
+                                    <p className="font-sans text-sm text-yellow-600">ID는 영어와 숫자만 가능하고 길이는 5~12에요!</p>
+                                    : null
+                            }
                         </div>
                         <div>
                             <input
@@ -86,9 +147,15 @@ const SignUp = () => {
                                 name="password"
                                 value={values.password}
                                 onChange={handleChange}
-                                className="h-12 gap-2 pl-2 my-3 font-mono text-lg border-b-2 border-gray-400 border-solid w-96"
-                                placeholder="Password"
+                                className={effectiveness.password ?
+                                    `${commonStyle} border-gray-400 border-solid` : `${commonStyle} border-red-700 border-solid`}
+                                placeholder='Password'
                             />
+                            {
+                                effectiveness.password === false ?
+                                    <p className="font-sans text-sm text-yellow-600">비밀번호는 영어와 숫자만 가능하고 8~12로 해주세요!</p>
+                                    : null
+                            }
                         </div>
                         <div>
                             <input
@@ -96,9 +163,15 @@ const SignUp = () => {
                                 name="verifyPassword"
                                 value={values.verifyPassword}
                                 onChange={handleChange}
-                                className="h-12 gap-2 pl-2 my-3 font-mono text-lg border-b-2 border-gray-400 border-solid w-96"
-                                placeholder="verifyPassword"
+                                className={effectiveness.verifyPassword ?
+                                    `${commonStyle} border-gray-400 border-solid` : `${commonStyle} border-red-700 border-solid`}
+                                placeholder='VerifyPassword'
                             />
+                            {
+                                effectiveness.verifyPassword === false ?
+                                    <p className="font-sans text-sm text-yellow-600">비밀번호를 한 번 더 입력해 주세요</p>
+                                    : null
+                            }
                         </div>
                         <div>
                             <input
@@ -106,40 +179,60 @@ const SignUp = () => {
                                 name="nickName"
                                 value={values.nickName}
                                 onChange={handleChange}
-                                className="h-12 gap-2 pl-2 my-3 font-mono text-lg border-b-2 border-gray-400 border-solid w-96"
-                                placeholder="Nickname"
+                                className={effectiveness.nickName ?
+                                    `${commonStyle} border-gray-400 border-solid` : `${commonStyle} border-red-700 border-solid`}
+                                placeholder='Nickname'
                             />
+                            {
+                                effectiveness.nickName === false ?
+                                    <p className="font-sans text-sm text-yellow-600">닉네임은 두 글자 이상으로 정해주세요</p>
+                                    : null
+                            }
                         </div>
-                        <div>
-                            <input
-                                type="text"
-                                name="eMail"
-                                value={values.eMail}
-                                onChange={handleChange}
-                                onKeyDown={onEnter}
-                                className="h-12 gap-2 pl-2 my-3 font-mono text-lg border-b-2 border-gray-400 border-solid w-96"
-                                placeholder="E-mail"
-                            />
-                        </div>
-                        <div className="flex ml-48 space-x-3 text-base text-slate-600">
-                            <p className="border-b-2 border-slate-300">
-                                아이디 찾기
-                            </p>
-                            <p className="border-b-2 border-slate-300">
-                                비밀번호 찾기
-                            </p>
-                        </div>
-                        <button
-                            onClick={handleSubmit}
-                            className="mt-16 bg-black rounded-md h-14 text-slate-200 w-96 border-slate-300"
-                        >
-                            Sign Up
-                        </button>
-                        <div className="flex">
-                            <p className="mt-3 text-sm border-b-2 ml-80 border-slate-300">
-                                고객센터
-                            </p>
-                        </div>
+                    </div>
+                    <div>
+                        <input
+                            type="text"
+                            name="eMail"
+                            value={values.eMail}
+                            onChange={handleChange}
+                            onKeyDown={onEnter}
+                            className={effectiveness.eMail ?
+                                `${commonStyle} border-gray-400 border-solid` : `${commonStyle} border-red-700 border-solid`}
+                            placeholder='E-mail'
+                        />
+                        {
+                            effectiveness.eMail === false ?
+                                <p className="font-sans text-sm text-yellow-600">이메일을 정확하게 적어주세요!</p>
+                                : null
+                        }
+                    </div>
+                    <div className="flex ml-48 space-x-3 text-base text-slate-600">
+                        <p className="border-b-2 border-slate-300">
+                            아이디 찾기
+                        </p>
+                        <p className="border-b-2 border-slate-300">
+                            비밀번호 찾기
+                        </p>
+                    </div>
+                    {
+                        (effectiveness.id && effectiveness.password && effectiveness.verifyPassword && effectiveness.nickName && effectiveness.eMail) ?
+                            <div className="mt-10 text-xl font-sans text-stone-500 space-y-2">
+                                <p>이제 WeRo의 회원이 될 준비가 끝났어요!</p>
+                                <p>아래 버튼을 눌러 WeRo의 회원이 되어보세요!</p>
+                            </div>
+                            : null
+                    }
+                    <button
+                        onClick={handleSubmit}
+                        className="mt-16 bg-black rounded-md h-14 text-slate-200 w-96 border-slate-300"
+                    >
+                        Sign Up
+                    </button>
+                    <div className="flex mb-20">
+                        <p className="mt-3 text-sm border-b-2 ml-80 border-slate-300">
+                            고객센터
+                        </p>
                     </div>
                 </div>
             </div>

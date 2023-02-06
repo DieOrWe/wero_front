3a8ae5036ebfdd4c7739328b3745ca5a249e41e0
unknown <dlscksgnl@naver.com>
2023-02-06 15:30:46 +0900

    delete user

--- src/components/setting_page/MyPage.tsx
+++ src/components/setting_page/MyPage.tsx
@@ -8,7 +8,6 @@ const MyPage = () => {
         setShow(true);
     }
     const [values, setValues] = useState({
-        id: '',
         password: '',
     })
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -17,53 +16,74 @@ const MyPage = () => {
             [e.target.name]: e.target.value,
         })
     }
-    const handleDelete = () => { }
+    const deleteUserUrl = "http://localhost:8080/api/user/data";
+    const handleDelete = async () => {
+        await fetch(deleteUserUrl + `/${localStorage.getItem('user_id')}`, {
+            method: "DELETE",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${localStorage.getItem("token")}`
+            },
+            body: values.password,
+        })
+        .then(resp => resp.json())
+        .then(resp => {
+            if (resp.message === "회원탈퇴가 성공적으로 이루어졌습니다.") {
+                alert(resp.message);
+                localStorage.removeItem('user_id');
+                localStorage.removeItem('token');
+                document.location.href = '/';
+            } else {
+                alert(resp.message);
+            }
+        })
+    }
     const post = 0;
     const get = 0;
     return (
         <div>
-            <div className='flex flex-col mt-8 mx-20'>
+            <div className='flex flex-col mx-20 mt-8'>
                 <div className='flex items-center mb-6'>
                     <img className='w-7 h-7 md:w-auto md:h-auto' src="img/PersonalSettings.png" alt="" />
-                    <p className='text-lg md:text-xl font-bold ml-1'>회원정보</p>
-                    <img className='mx-1 w-2 md:w-3' src="img/arrow.png" alt="" />
+                    <p className='ml-1 text-lg font-bold md:text-xl'>회원정보</p>
+                    <img className='w-2 mx-1 md:w-3' src="img/arrow.png" alt="" />
                     <img className='mx-1 w-7 h-7 md:w-auto md:h-auto' src="img/MyPage.png" alt="" />
-                    <p className='text-lg md:text-xl font-bold ml-1'>마이페이지</p>
+                    <p className='ml-1 text-lg font-bold md:text-xl'>마이페이지</p>
                 </div>
-                <div className='mb-7 w-28 h-8 md:w-40 md:h-10 rounded-lg bg-gray-100 items-center'>
-                    <Link to={'/setting/userInfo'} className='flex justify-between items-center w-full h-full'>
+                <div className='items-center h-8 bg-gray-100 rounded-lg mb-7 w-28 md:w-40 md:h-10'>
+                    <Link to={'/setting/userInfo'} className='flex items-center justify-between w-full h-full'>
                         <img src="img/leftArrow.png" alt="" />
                         <p>뒤로</p>
                         <p> </p>
                     </Link>
                 </div>
-                <div className='flex justify-between border-b-2 h-10 text-lg mb-5'>
+                <div className='flex justify-between h-10 mb-5 text-lg border-b-2'>
                     <p>내가 쓴 편지의 수:</p>
                     <div className='flex space-x-2'>
                         <p>{post}</p>
                         <p>통</p>
                     </div>
                 </div>
-                <div className='flex justify-between border-b-2 h-10 text-lg'>
+                <div className='flex justify-between h-10 text-lg border-b-2'>
                     <p>내가 받은 편지의 수:</p>
                     <div className='flex space-x-2'>
                         <p>{get}</p>
                         <p>통</p>
                     </div>
                 </div>
-                <div className='mt-10 place-self-center w-28 h-8 md:w-40 md:h-10 rounded-lg bg-gray-100 flex items-center justify-center'>
-                    <p onClick={handleShow}>탈퇴하기</p>
+                <div onClick={handleShow} className='flex items-center justify-center h-8 mt-10 bg-gray-100 rounded-lg place-self-center w-28 md:w-40 md:h-10'>
+                    <p>탈퇴하기</p>
                 </div>
             </div>
             <div className='mb-56'></div>
 
             {show && (<div className='modal'>
                 <div className='overlay'>
-                    <div className="modal-content">
-                        <input className='w-56 h-8 mb-3' onChange={handleChange} value={values.id} name='id' type="text" placeholder='id' />
+                    <div style={{ width: "300px" }} className="modal-content">
+                        <input className='w-56 h-8 mb-3' type="text" value={localStorage.getItem('user_id')!} readOnly />
                         <br />
-                        <input className='w-56 h-8 mb-20' onChange={handleChange} value={values.password} name='password' type="text" placeholder='password' />
-                        <button className="close-modal mb-10 bg-black text-white rounded-xl" onClick={handleDelete}>
+                        <input className='w-56 h-8 mb-20' onChange={handleChange} value={values.password} name='password' type="password" placeholder='password' />
+                        <button className="mb-10 text-white bg-black close-modal rounded-xl" onClick={handleDelete}>
                             탈퇴하기
                         </button>
                         <button className="close-modal bg-slate-200 rounded-xl" onClick={handleClose}>

236f9acd2cf2fc271e8659dc20ad17fd1602cf0c
wassaa-1 <dlscksgnl@naver.com>
2023-02-14 12:10:44 +0900

    alarm

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,30 +1,24 @@
-import React, { SetStateAction, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react';
 import { HashRouter, Link, Route, Routes } from 'react-router-dom';
 import LetterStorge from './letter_storge/LetterStorge';
 import FirstSettingPage from './setting_page/FirstSettingPage';
 import WriteLetter from './write_letter/WriteLetter';
-import SockJS from "sockjs-client";
-import Stomp from "stompjs";
 
 const MainPage = () => {
-    const [stompClient, setStompClient] = useState<any>(null);
-    const [connected, setConnected] = useState(false);
-    const [greetings, setGreetings] = useState([]);
-
+    const [notify, setNotify] = useState('Notification');
     useEffect(() => {
-        const socket = new SockJS("http://localhost:8080/stomp-end-websocket");
-        const client = Stomp.over(socket);
-        client.connect({}, (frame) => {
-            setConnected(true);
-            console.log("Connected: " + frame);
-            client.subscribe("/topic/backmessage", (greeting) => {
-                console.log(JSON.parse(greeting.body).content);
-            });
-            setStompClient(client);
-        });
-    }
-        , [])
+        const intervalId = setInterval(() => {
+            const currentDate = new Date();
+            const currentHour = currentDate.getHours();
+            const currentMinute = currentDate.getMinutes();
+
+            if ((currentHour === 12 && currentMinute === 9) || (currentHour === 0 && currentMinute === 0)) {
+                setNotify('Notification_notified_btm')
+            }
+        }, 60000); // 매 분마다 호출
+        return () => clearInterval(intervalId);
+    }, [])
 
     const [topButton, setTopButton] = useState({
         email: '_clicked',
@@ -33,6 +27,9 @@ const MainPage = () => {
     })
     const handleClick = (e: React.MouseEvent) => {
         const target = e.target as Element;
+        if (target.className === 'email') {
+            setNotify('Notification')
+        }
         setTopButton({
             email: '',
             write: '',
@@ -56,7 +53,7 @@ const MainPage = () => {
                         <Link to='/'><button className='w-7 h-7 md:w-auto md:h-auto' onClick={handleClick}><img className='email' src={`img/Email${topButton.email}.png`} alt="email" /></button></Link>
                         <Link to='/write'><button className='w-7 h-7 md:w-auto md:h-auto' onClick={handleClick}><img className='write' src={`img/Write${topButton.write}.png`} alt="write" /></button></Link>
                         <Link to='/setting'><button className='w-7 h-7 md:w-auto md:h-auto' onClick={handleClick}><img className='setting' src={`img/Setting${topButton.setting}.png`} alt="setting" /></button></Link>
-                        <Link to='/'><button className='w-7 h-7 md:w-auto md:h-auto' onClick={handleClick}><img className='email' src={`img/Notification.png`} alt="email" /></button></Link>
+                        <Link to='/'><button className='w-7 h-7 md:w-auto md:h-auto' onClick={handleClick}><img className='email' src={`img/${notify}.png`} alt="email" /></button></Link>
                         <img className='w-7 h-7' onClick={onLogout} src="img/Logout.png" alt="logout" />
                     </div>
                     <div>

681f8e2e5ad01838ccc15ca62cf0ee0aa6144b5f
wassaa-1 <dlscksgnl@naver.com>
2023-02-15 11:01:57 +0900

    asdf,,,

--- src/components/write_letter/WriteLetter.tsx
+++ src/components/write_letter/WriteLetter.tsx
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
 
 const WriteLetter = () => {
-    const BaseUrl = "/api/myLetter/createMyLetter";
-    const [letter, setLetter] = useState({
-        title: "",
-        content: "",
-        isCheck: false,
-    });
-  };
+  const BaseUrl = "/api/myLetter/createMyLetter";
+  const [letter, setLetter] = useState({
+    title: "",
+    content: "",
+    isCheck: false,
+  });
   const handleClick = () => {
     if (letter.content.length > 255) {
       alert('편지 내용은 최대 255자 만큼 쓸 수 있습니다.');
@@ -61,53 +60,53 @@ const WriteLetter = () => {
     });
   };
 
-    return (
-        <div>
-            <div className="flex mt-8 mb-3 ml-10 text-2xl font-bold md:text-3xl md:mt-14">
-                <img
-                    src="img/Write.png"
-                    alt=""
-                    className="w-8 h-8 ml-4 mr-2 md:ml-0 md:w-10 md:h-10 "
-                />
-                <h1>편지 쓰기</h1>
-            </div>
-            <div className="mx-20 mt-10 md:mt-16">
-                <input
-                    type="text"
-                    onChange={handleChange}
-                    name="title"
-                    className="w-full mb-10 border-b-2 border-slate-500"
-                    placeholder="제목"
-                />
-                <textarea
-                    onChange={handleChange}
-                    name="content"
-                    value={letter.content}
-                    className="w-full border-b-2 border-black h-96"
-                    placeholder="오늘의 이야기를 들려주세요. :)"
-                />
-                <div className="flex items-center justify-between mt-4 space-x-2">
-                    <div className="flex items-center">
-                        <input
-                            type="checkbox"
-                            onClick={handleCheck}
-                            className="w-4 h-4 mr-2"
-                            name=""
-                            id="open"
-                        />
-                        <label htmlFor="open">편지 공개</label>
-                    </div>
-                    <button
-                        onClick={handleClick}
-                        className="h-10 mt-16 bg-black right-20 md:h-12 w-28 md:w-52 rounded-2xl text-slate-200 border-slate-300"
-                    >
-                        전송
-                    </button>
-                </div>
-            </div>
-            <div className="h-40 md:h-16"></div>
+  return (
+    <div>
+      <div className="flex mt-8 mb-3 ml-10 text-2xl font-bold md:text-3xl md:mt-14">
+        <img
+          src="img/Write.png"
+          alt=""
+          className="w-8 h-8 ml-4 mr-2 md:ml-0 md:w-10 md:h-10 "
+        />
+        <h1>편지 쓰기</h1>
+      </div>
+      <div className="mx-20 mt-10 md:mt-16">
+        <input
+          type="text"
+          onChange={handleChange}
+          name="title"
+          className="w-full mb-10 border-b-2 border-slate-500"
+          placeholder="제목"
+        />
+        <textarea
+          onChange={handleChange}
+          name="content"
+          value={letter.content}
+          className="w-full border-b-2 border-black h-96"
+          placeholder="오늘의 이야기를 들려주세요. :)"
+        />
+        <div className="flex items-center justify-between mt-4 space-x-2">
+          <div className="flex items-center">
+            <input
+              type="checkbox"
+              onClick={handleCheck}
+              className="w-4 h-4 mr-2"
+              name=""
+              id="open"
+            />
+            <label htmlFor="open">편지 공개</label>
+          </div>
+          <button
+            onClick={handleClick}
+            className="h-10 mt-16 bg-black right-20 md:h-12 w-28 md:w-52 rounded-2xl text-slate-200 border-slate-300"
+          >
+            전송
+          </button>
         </div>
-    );
+      </div>
+      <div className="h-40 md:h-16"></div>
+    </div>
+  );
 };
 
 export default WriteLetter;
